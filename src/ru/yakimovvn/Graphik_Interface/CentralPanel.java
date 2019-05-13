67e4d1cc8da9b8919ff5f84170a1dc2f93a32b0e
Vladimir Iakimov <yakimovvn@bk.ru>
2019-05-13 14:18:25 +0500

    Кнопка Close и константы

--- src/ru/yakimovvn/Graphik_Interface/CentralPanel.java
+++ src/ru/yakimovvn/Graphik_Interface/CentralPanel.java
@@ -3,4 +3,8 @@ package ru.yakimovvn.Graphik_Interface;
 import javax.swing.*;
 
 public class CentralPanel extends JPanel {
+
+    public CentralPanel(){
+
+    }
 }
--- src/ru/yakimovvn/Graphik_Interface/MyFrame.java
+++ src/ru/yakimovvn/Graphik_Interface/MyFrame.java
@@ -4,12 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyFrame extends JFrame {
+
+    static final String TRIP_FILE = "./img/";
+    CentralPanel panel;
+
     public MyFrame(String windowName){
         super(windowName);
         setResizable(true);
         setLayout(new BorderLayout());
         setBounds(250,250,500,300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+        JButton closeButton = new JButton("CLOSE");
+        closeButton.setSize(new Dimension(100,30));
+        closeButton.addActionListener(e -> System.exit(0));
+        closeButton.setFocusPainted(false);
+        closeButton.setOpaque(true);
+        closeButton.setContentAreaFilled(true);
+        add(closeButton,BorderLayout.SOUTH);
 
         setVisible(true);
 
